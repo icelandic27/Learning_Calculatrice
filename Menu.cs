5a52f024e71a3aa9e9d4abc1ecd46d66c9bfb374
icelandic27 <115848447+icelandic27@users.noreply.github.com>
2022-10-27 19:49:52 -0400

    Classe menu crée + Ajoutez des fichiers projet.

---
+++ Menu.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learning_Calculatrice
+{
+    internal class Menu
+    {
+
+        
+
+        //public List<string> Selection { get; set; }
+
+
+        public static int ShowInformations()
+        {
+            List<string> selection = new List<string> { "Addition", "Multiplication", "Division", "Soustraction" };
+            int index = 1;
+            Console.WriteLine("Calculateur Console");
+            foreach (var x in selection)
+            {
+                Console.WriteLine($"\tOption {index++} : {x}");
+            }
+            Console.Write("Votre choix : ");
+            int choix = Convert.ToInt32(Console.ReadLine());
+            return choix;
+        }
+
+
+
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿using Learning_Calculatrice;
+
+
+Menu.ShowInformations();
+
+
